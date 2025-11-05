f8a709a7ccc3bbe879d266e29330bd6b5d63ffdd
paramjeet.singh.ext@essity.com <paramjeet.singh.ext@essity.com>
2025-11-05 14:54:26 +0000

    exploring get post delete api calls

--- src/app.js
+++ src/app.js
@@ -2,14 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req,res)=>{
+app.use("/user", (req, res) => {
     res.send("you have reached test route");
-})
-
-app.use((req,res)=>{
-   res.send("Hello from Port 8000");
 });
 
-app.listen(8000,()=>{
+app.get("/user", (req, res) => {
+    res.send({ firstname: "Param", lastname: "singh" });
+});
+
+app.post("/user", (req, res) => {
+    res.send("Data saved successfully yo post call");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully");
+});
+
+
+// this will match all the http methods of api calls to /test
+app.use("/test", (req, res) => {
+    res.send("you have reached test route");
+});
+
+
+app.listen(8000, () => {
     console.log("Server is connected to Port 8000 successfully");
 });
