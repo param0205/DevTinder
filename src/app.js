405b71201e1dedab2e28962594b12340467c488a
paramjeet.singh.ext@essity.com <paramjeet.singh.ext@essity.com>
2025-11-05 14:59:07 +0000

    route

--- src/app.js
+++ src/app.js
@@ -2,26 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res) => {
-    res.send("you have reached test route");
-});
-
-app.get("/user", (req, res) => {
+app.get("/ab?c", (req, res) => {
     res.send({ firstname: "Param", lastname: "singh" });
-});
-
-app.post("/user", (req, res) => {
-    res.send("Data saved successfully yo post call");
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("deleted successfully");
-});
-
-
-// this will match all the http methods of api calls to /test
-app.use("/test", (req, res) => {
-    res.send("you have reached test route");
 });
 
 
