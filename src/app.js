53225515da67a349f2226bd90ebe87ba01be0e9a
paramjeet.singh.ext@essity.com <paramjeet.singh.ext@essity.com>
2025-11-13 18:11:37 +0000

    Need of middleware and exp

--- src/app.js
+++ src/app.js
@@ -4,30 +4,29 @@ const res = require("express/lib/response");
 const app = express();
 
 
-app.use("/middleware", (req, res, next) => {
-    console.log("Handling the route handler 1");
-    next();
-    res.send("Found thr middleware 1");
-}, (req, res,next) => {
-    console.log("Handling the route handler 2");
-    // res.send("found the middleware 2");
-    next();
-},(req, res,next) => {
-    console.log("Handling the route handler 3");
-    // res.send("found the middleware 3");
-    next();
-}
+//when we call an express api it goes through all the matching routes untill it sends back the response or times out
+// in actual it goes through all the middleware chain before actually reaching to route handlers which sends us back the response
+// route handler and middleware can be referred as same depending upon the case
+
+
+app.use("/middleware",
+    [(req, res, next) => {
+        console.log("Handling the route handler 1");
+        next();
+        // res.send("Found the middleware 1");
+    },]
 )
 
-app.get('/user/:userid/:name/:password', (req, res) => {
-    console.log(req.params);
-    res.send({ firstname: "Param", lastname: "singh" });
-});
-
-app.get("/abc", (req, res) => {
-    res.send({ firstname: "Paam", lastname: "singh" });
-});
-
+app.use("/middleware",
+    [(req, res, next) => {
+        console.log("Handling the route handler 2");
+        // next();
+        res.send("Found the middleware 12");
+    }, (req, res, next) => {
+        console.log("Handling the route handler 3");
+        next();
+        res.send("Found the middleware 2");
+    }])
 
 app.listen(8000, () => {
     console.log("Server is connected to Port 8000 successfully");
