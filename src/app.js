224a5e8b7c65709cd815241b1f63a17c215b4178
paramjeet.singh.ext@essity.com <paramjeet.singh.ext@essity.com>
2025-11-13 11:49:37 +0000

    middleware and next method

--- src/app.js
+++ src/app.js
@@ -1,13 +1,30 @@
 const express = require("express");
+const res = require("express/lib/response");
 
 const app = express();
+
+
+app.use("/middleware", (req, res, next) => {
+    console.log("Handling the route handler 1");
+    next();
+    res.send("Found thr middleware 1");
+}, (req, res,next) => {
+    console.log("Handling the route handler 2");
+    // res.send("found the middleware 2");
+    next();
+},(req, res,next) => {
+    console.log("Handling the route handler 3");
+    // res.send("found the middleware 3");
+    next();
+}
+)
 
 app.get('/user/:userid/:name/:password', (req, res) => {
     console.log(req.params);
     res.send({ firstname: "Param", lastname: "singh" });
 });
 
-app.get("/abc/test", (req, res) => {
+app.get("/abc", (req, res) => {
     res.send({ firstname: "Paam", lastname: "singh" });
 });
 
