2a3fa2c06f7303b2551bd339406938d917533cc2
paramjeet.singh.ext@essity.com <paramjeet.singh.ext@essity.com>
2025-11-25 20:04:30 +0000

    user feed API and skip top

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -4,6 +4,8 @@ const { ConnectionRequest } = require("../models/connectionRequest");
 const express = require("express");
 const userRouter = express.Router();
 const USER_INFO = ["firstName", "lastName", "photoUrl", "gender", "age", "skills", "about"];
+const User = require("../models/user");
+
 
 userRouter.get("/user/requests/recevied", userAuth, async (req, res) => {
     try {
@@ -41,18 +43,64 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
                 }
             ]
 
-        }).populate("fromUserId", USER_INFO).populate("toUserId",USER_INFO );
+        }).populate("fromUserId", USER_INFO).populate("toUserId", USER_INFO);
 
         console.log(connectionRequests);
 
         const data = connectionRequests.map((user) => {
-            if(loggedInUser._id.toString() === user.fromUserId._id.toString())
+            if (loggedInUser._id.toString() === user.fromUserId._id.toString())
                 return user.toUserId;
-            return user.fromUserId ;
+            return user.fromUserId;
         })
         res.send({
             message: `Connection requests accepted`,
             data
+        });
+
+    } catch (err) {
+        res.status(400).send("Error :" + err.message);
+    }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page-1)*limit;    
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {
+                    toUserId: loggedInUser._id
+                },
+                {
+                    fromUserId: loggedInUser._id
+                }
+            ]
+        }).select("fromUserId toUserId")
+
+        console.log(connectionRequests);
+
+        const data = connectionRequests.map((user) => {
+            if (loggedInUser._id.toString() === user.fromUserId._id.toString())
+                return user.toUserId;
+            return user.fromUserId;
+        });
+
+        const userFeed = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(data) } },
+                {_id: { $ne: loggedInUser._id }}
+            ]
+        }).select(USER_INFO)
+        .skip(skip)
+        .limit(limit);
+
+        res.send({
+            message: `User Data feed fetched Successfully`,
+            userFeed
         });
 
     } catch (err) {
